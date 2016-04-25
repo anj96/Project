a3defe2270f1c7746f5ffd72090961d192ad8602
anj96 <mittalanjali1996@gmail.com>
2016-04-25 09:28:07 +0530

    Database

--- app/src/main/java/com/example/anjali/project/DBHelper.java
+++ app/src/main/java/com/example/anjali/project/DBHelper.java
@@ -3,60 +3,69 @@ package com.example.anjali.project;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by Anjali on 10-04-2016.
  */
-public class DBHelper extends SQLiteOpenHelper{
-    public static final String DATABASE_NAME = "info.db";
+public class DBHelper {
+    public static final String DATABASE_NAME = "Student_Exam";
     public static final int DATABASE_VERSION= 1;
     public static final String TABLE_NAME= "Resigter";
     public static final String KEY_ID= "_id";
-    //public static final String COL_NAME = "Name";
-    //public static final int COL_INDEX1 = "_id";
-    //public static final String COL_NAME1 = "Profession";
     public static final String KEY_NAME = "studentname";
-public static final String KEY_PROFESSION = "studentprofession";
-    private SQLiteDatabase db;
+    public static final String KEY_EMAIL = "studentemail";
+    public static final String KEY_PASSWORD = "studentpassword";
+    public static final String KEY_PROFESSION = "studentprofession";
 
 
 
     private static final String DB_CREATE="create table "+TABLE_NAME+" ("
             +KEY_ID+" integer primary key "+ "autoincrement , " +
-            ""+KEY_NAME+" text not null, "  +KEY_PROFESSION+" text not null);";
+            ""+KEY_NAME+" text not null, "
+            + KEY_EMAIL+" text not null, "
+            + KEY_PASSWORD+" text not null, "
+            +KEY_PROFESSION+" text not null);";
+
+    private SQLiteDatabase db;
+    private final Context context;
+    private MyDBAdapter helper;
 
 
-
-    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
-
+    public DBHelper (Context context)
+    {
+        this.context = context;
+        helper = new MyDBAdapter(context,DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL(DB_CREATE);
-
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
-        onCreate(db);
-
+    public DBHelper open(){
+        db = helper.getWritableDatabase();
+        return this;
     }
 
 
-    public void insertEntry(String name, String profession) {
+    public void close()
+    {
+        db.close();
+    }
+
+
+
+    public void insertEntry(String name,String email,String password ,String profession) {
+
+        SQLiteDatabase db = helper.getWritableDatabase();
 
         ContentValues contentValues=new ContentValues();
         contentValues.put(KEY_NAME,name);
-        contentValues.put(KEY_PROFESSION,profession);
+        contentValues.put(KEY_EMAIL,email);
+        contentValues.put(KEY_PASSWORD,password);
+        contentValues.put(KEY_PROFESSION, profession);
 
-        SQLiteDatabase db = getWritableDatabase();
         db.insert(TABLE_NAME, null, contentValues);
         db.close();
     }
@@ -65,16 +74,42 @@ public static final String KEY_PROFESSION = "studentprofession";
 
     public void deleteEntry(String studentname){
 
-        SQLiteDatabase db = getWritableDatabase();
+        SQLiteDatabase db = helper.getWritableDatabase();
         db.execSQL("DELETE FROM " + TABLE_NAME + "WHERE " + KEY_NAME + "=\"" + studentname + "\";");
+
+    }
+
+    public boolean verifyUser(String email, String password){
+        String[] verify = new String[2];
+        Cursor  cur= db.rawQuery("select " + KEY_EMAIL+","+ KEY_PASSWORD+" from "+TABLE_NAME, null);
+        cur.moveToFirst();
+        verify[0] = cur.getString(cur.getColumnIndex(KEY_EMAIL));
+        verify[1] = cur.getString(cur.getColumnIndex(KEY_PASSWORD));
+        while(cur.moveToNext())
+        {
+            verify[0] = cur.getString(cur.getColumnIndex(KEY_EMAIL));
+            verify[1] = cur.getString(cur.getColumnIndex(KEY_PASSWORD));
+            if(verify[0].equals(email) || verify[1].equals(password)) {
+                break;
+            }
+
+        }
+        cur.close();
+        if(verify[0].equals(email) && verify[1].equals(password)){
+            return true;
+        }
+        else {
+            return false;
+        }
 
     }
 
 
 
+
     public String getEntry(){
         String dbString="";
-        SQLiteDatabase db= getWritableDatabase();
+        SQLiteDatabase db= helper.getWritableDatabase();
         String query = "SELECT studentname FROM "+ TABLE_NAME+ " WHERE " +KEY_ID+ " = 1";
         Cursor c = db.rawQuery(query,null);
         c.moveToFirst();
@@ -94,6 +129,28 @@ public static final String KEY_PROFESSION = "studentprofession";
 
         db.close();*/
         return dbString;
+    }
+    private static class MyDBAdapter extends SQLiteOpenHelper {
+
+        public MyDBAdapter(Context context, String name,
+                           SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+            sqLiteDatabase.execSQL(DB_CREATE);
+            }
+
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+            Log.w("Updation", "Data base version is being updates");
+            sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + DB_CREATE);
+            onCreate(sqLiteDatabase);
+        }
+
     }
 
 
--- app/src/main/java/com/example/anjali/project/RegisterActivity.java
+++ app/src/main/java/com/example/anjali/project/RegisterActivity.java
@@ -1,6 +1,5 @@
 package com.example.anjali.project;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,8 +12,8 @@ import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     Button submitButton;
-    EditText nameText,professionText;
-    TextView enterName;
+    EditText nameText,emailText,passwordText,professionText;
+    TextView enterName,enterEmail, enterPassword, enterProfession;
     TextView databaseValue;
     DBHelper dbHelper;
 
@@ -26,10 +25,15 @@ public class RegisterActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         nameText = (EditText)findViewById(R.id.nameText);
-        professionText = (EditText)findViewById(R.id.professionText);
+        professionText = (EditText)findViewById(R.id.emailText);
+        emailText = (EditText)findViewById(R.id.emailText);
+        passwordText = (EditText)findViewById(R.id.passwordText);
         enterName = (TextView)findViewById(R.id.enterName);
+        enterEmail = (TextView)findViewById(R.id.enterEmail);
+        enterPassword = (TextView)findViewById(R.id.enterPassword);
+        enterProfession = (TextView)findViewById(R.id.enterPassword);
         databaseValue = (TextView)findViewById(R.id.databaseValue);
-        dbHelper= new DBHelper(RegisterActivity.this, null,null,1);
+        dbHelper= new DBHelper(getBaseContext());
        // printDatabase();
 
 
@@ -37,7 +41,7 @@ public class RegisterActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
 
-                dbHelper.insertEntry(nameText.getText().toString(),professionText.getText().toString());
+                dbHelper.insertEntry(nameText.getText().toString(),emailText.getText().toString(),passwordText.getText().toString(),professionText.getText().toString());
                 printDatabase();
                // Intent i = new Intent(RegisterActivity.this, Registered.class);
                 //startActivity(i);
